0daf484e7fb784357bf0cb9af3160005d1fda41b
Thiago Tolotti <thiago.tolotti@riole.com.br>
2026-03-17 12:54:07 -0300

    fixes /arquivo/{id} for full redirection rules

--- src/app/api/routes/default.py
+++ src/app/api/routes/default.py
@@ -2,7 +2,7 @@ from uuid import UUID
 
 from fastapi import UploadFile
 from fastapi.routing import APIRouter
-from fastapi.responses import Response
+from fastapi.responses import RedirectResponse
 
 from app import services
 
@@ -15,16 +15,14 @@ def ping():
 
 
 @router.get("/arquivo/{id}")
-def get_arquivo(id: UUID, response: Response):
-    file = services.file.get(id)
+def get_arquivo(id: UUID):
+    path = services.file.get(id)
 
-    if file is None:
-        response.status_code = 404
-        return {"message": "Arquivo não encontrado"}
+    return RedirectResponse(str(path))
 
 
 @router.post("/arquivo")
-async def upload_arquivo(file: UploadFile, response: Response):
+async def upload_arquivo(file: UploadFile):
     id = await services.file.upload(file)
 
     return {"message": "O upload foi bem sucedido", "id": str(id)}
--- src/app/services/file.py
+++ src/app/services/file.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import shutil
 
 from uuid import UUID, uuid4
@@ -7,13 +8,35 @@ from fastapi.exceptions import HTTPException
 from app.constants import STATIC_PATH
 
 
-def get(id: UUID) -> None:
-    return None
+class FileNotFoundException(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=404, detail="Arquivo não encontrado")
+
+
+def get(id: UUID) -> str:
+    target_path = STATIC_PATH / str(id)
+
+    if not target_path.is_dir():
+        raise FileNotFoundException
+
+    files = [f for f in target_path.iterdir() if f.is_file()]
+
+    match len(files):
+        case 0:
+            raise FileNotFoundException
+        case 1:
+            web_path = Path("static") / str(id) / files[0].name
+
+            return f"/{web_path.as_posix()}"
+        case _:
+            raise HTTPException(
+                status_code=422, detail="Múltiplos arquivos encontrados."
+            )
 
 
 async def upload(file: UploadFile) -> UUID | None:
     if not file.filename:
-        raise HTTPException(status_code=400, detail="No ")
+        raise HTTPException(status_code=400, detail="Arquivo sem nome.")
 
     id = uuid4()
 
