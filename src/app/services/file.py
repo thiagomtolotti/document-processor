799538087c6c85f98cd11600825614c2ee5c11c3
Thiago Tolotti <thiago.tolotti@riole.com.br>
2026-03-17 11:32:18 -0300

    creates files path if not exists, adds STATIC_PATH constant

--- src/app/services/file.py
+++ src/app/services/file.py
@@ -1,7 +1,11 @@
 import shutil
+
 from uuid import UUID, uuid4
+from pathlib import Path
 
 from fastapi.datastructures import UploadFile
+
+STATIC_PATH = Path("./src/app/static")
 
 
 def get(id: UUID) -> None:
@@ -12,10 +16,12 @@ async def upload(file: UploadFile):
     if not file.filename:
         return
 
-    extension = file.filename.split(".")[-1]
     id = uuid4()
 
-    target_path = f"./src/app/static/{id}.{extension}"
+    target_path = STATIC_PATH / str(id)
+    file_path = target_path / file.filename
 
-    with open(target_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+    target_path.mkdir(parents=True)
+
+    with file_path.open("w") as f:
+        shutil.copyfileobj(file.file, f.buffer)
